20ca26369c663b204a93b6716cf940205a1da0cb
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 17:07:14 +1000

    Create factorial.hpp

---
+++ Data Structures and Algorithms/Tutorial 2/Factorial/factorial.hpp
@@ -0,0 +1,11 @@
+// header file
+#ifndef FACTORIAL_HPP
+#define FACTORIAL_HPP
+
+// declaration of the factorial function
+// this gives the signature of the function
+// you do not need to do anything here
+
+int factorial(int);
+
+#endif  // FACTORIAL_HPP
