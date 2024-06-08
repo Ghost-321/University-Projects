5c29cfac79d380102ba156b48c12f9ff441c7c07
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 17:20:29 +1000

    Create myVector.cpp

---
+++ Data Structures and Algorithms/Tutorial 5/Adding Iterators to MyVector/myVector.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <initializer_list>
+#include <utility>
+#include <string>
+#include "myVector.hpp"
+
+template <typename T>
+MyVector<T>::MyVector() {}
+
+template <typename T>
+MyVector<T>::MyVector(int n) :
+  arrayPointer_ {n > 0 ? new T[n]{} : nullptr},
+  size_ {n},
+  capacity_ {n} {}
+
+template <typename T>
+MyVector<T>::MyVector(const MyVector<T>& other)
+    : arrayPointer_ {other.size_ > 0 ? new T[other.size_] : nullptr},
+      size_ {other.size_},
+      capacity_ {other.capacity_} {
+  for (int i = 0; i < size_; ++i) {
+    arrayPointer_[i] = other.arrayPointer_[i];
+  }
+}
+
+template <typename T>
+MyVector<T>::MyVector(std::initializer_list<T> vals) {
+  size_ = capacity_ = vals.size();
+  arrayPointer_ = new T[size_];
+  int i = 0;
+  for (T x : vals) {
+    arrayPointer_[i++] = x;
+  }
+}
+
+template <typename T>
+MyVector<T>::~MyVector() {
+  delete[] arrayPointer_;
+}
+
+template <typename T>
+MyVector<T>& MyVector<T>::operator=(MyVector<T> other) {
+  std::swap(arrayPointer_, other.arrayPointer_);
+  std::swap(size_, other.size_);
+  std::swap(capacity_, other.capacity_);
+  return *this;
+}
+
+template <typename T>
+void MyVector<T>::push_back(T val) {
+  if (size_ < capacity_) {
+    arrayPointer_[size_++] = val;
+  } else {
+    capacity_ = (size_ == 0 ? 1 : 2*size_);
+    T* temp = new T[capacity_];
+    for (int i = 0; i < size_; ++i) {
+      temp[i] = arrayPointer_[i];
+    }
+    temp[size_++] = val;
+    delete[] arrayPointer_;
+    arrayPointer_ = temp;
+    temp = nullptr;
+  }
+}
+
+template <typename T>
+void MyVector<T>::pop_back() {
+  if (!empty()) {
+    arrayPointer_[size_-1].~T();
+    --size_;
+  }
+}
+
+template <typename T>
+T& MyVector<T>::back() {
+  return arrayPointer_[size_-1];
+}
+
+template <typename T>
+bool MyVector<T>::empty() {
+  return size_ == 0;
+}
+
+template <typename T>
+int MyVector<T>::size() const {
+  return size_;
+}
+
+template <typename T>
+int MyVector<T>::capacity() const {
+  return capacity_;
+}
+
+template <typename T>
+T& MyVector<T>::operator[](int i) {
+  return arrayPointer_[i];
+}
+
+template <typename T>
+const T& MyVector<T>::operator[](int i) const {
+  return arrayPointer_[i];
+}
+
+template <typename T>
+typename MyVector<T>::Iterator MyVector<T>::begin() const {
+  return Iterator {arrayPointer_};
+}
+
+template <typename T>
+typename MyVector<T>::Iterator MyVector<T>::end() const {
+  return Iterator {arrayPointer_ + size_};
+}
+
+// member functions of MyVector<T>::Iterator
+template <typename T>
+MyVector<T>::Iterator::Iterator(T* input) : ptr {input} {}
+
+template <typename T>
+T& MyVector<T>::Iterator::operator*() {
+  return *ptr;
+}
+
+template <typename T>
+typename MyVector<T>::Iterator& MyVector<T>::Iterator::operator++() {
+  ++ptr;
+  return *this;
+}
+
+template <typename T>
+typename MyVector<T>::Iterator& MyVector<T>::Iterator::operator--() {
+  --ptr;
+  return *this;
+}
+
+
+template class MyVector<int>;
+template class MyVector<double>;
+template class MyVector<std::string>;
