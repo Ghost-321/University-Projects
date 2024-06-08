e5b42295944e16ffe763fb4ce92695aaccf13305
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 16:36:20 +1000

    Create Arithmetric.java

---
+++ Programming 1/Lab-Work/Arithmetric.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value for a: ");
+		int a = sc.nextInt();
+		System.out.print("Enter the value for b: ");
+		int b = sc.nextInt();
+		
+		//Add your code starting here.
+		
+	}
+	
+}
