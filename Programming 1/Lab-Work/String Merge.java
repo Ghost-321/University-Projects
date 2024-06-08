cea09c8d4fc1c8a2a865d0230f18334645c30405
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 16:39:00 +1000

    Create String Merge.java
    Merging Strings
    This time, the skeleton reads in two Strings from the user. Concatenate these strings and print out the result.

---
+++ Programming 1/Lab-Work/String Merge.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StringMerge {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String t = sc.nextLine();
+		String c = s+t;
+		System.out.println(c);
+		//Add your code starting here.
+		
+	}
+	
+}
