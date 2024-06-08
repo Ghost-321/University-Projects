3d9ba1e0ac14ebba7d9c738c629eff23f7fcf7da
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 16:37:42 +1000

    Update Arithmetric.java
    All the Arithmetic
    You have the skeleton of a small program which reads in two values from the user, and store them in variables a and b.
    
    Your job is to add some code that computes a + b, a - b, a  *  b,a / b and a % b, stores them in separate variables, and prints them out to separate lines.

--- Programming 1/Lab-Work/Arithmetric.java
+++ Programming 1/Lab-Work/Arithmetric.java
@@ -9,9 +9,17 @@ public class Arithmetic {
 		int a = sc.nextInt();
 		System.out.print("Enter the value for b: ");
 		int b = sc.nextInt();
-		
-		//Add your code starting here.
-		
+		int c = a+b;
+		int d = a-b;
+		int e = a*b;
+		int f = a/b;
+		int g = a%b;
+		//Add your code starting here.	
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
 	}
 	
 }
