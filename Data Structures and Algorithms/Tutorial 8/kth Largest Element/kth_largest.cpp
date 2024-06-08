fe75659cbfbfd194bb93ef3185216720e32ffb95
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 17:29:16 +1000

    Update kth_largest.cpp

--- Data Structures and Algorithms/Tutorial 8/kth Largest Element/kth_largest.cpp
+++ Data Structures and Algorithms/Tutorial 8/kth Largest Element/kth_largest.cpp
@@ -4,5 +4,13 @@
 #include "kth_largest.hpp"
 
 int kthLargest(const std::vector<int>& vec, int k) {
-  return 0;
+  std::priority_queue<int, std::vector<int>,
+                      std::greater<int> > pq {};
+  for (int x : vec) {
+    pq.push(x);
+    if (static_cast<int>(pq.size()) > k) {
+      pq.pop();
+    }
+  }
+  return pq.top();
 }
