0e78dc57e7d8b06cba67d69ee2c17c622c892204
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 16:41:54 +1000

    Create if-else.java

---
+++ Programming 1/Lab-Work/if-else.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Carpark {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		//Your code goes here!
+		if ( n >= 37) {
+			System.out.println ("Heads");
+
+		}
+
+		else {
+		System.out.println ("Tails");
+		}
+	}
+}
