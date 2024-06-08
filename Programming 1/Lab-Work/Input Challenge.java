d7fc58a2c0369b007e695c2b52a6d44e77183d9b
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 16:39:48 +1000

    Create Input Challenge.java
    A Little Input Challenge
    So we've had a few programs now that read something in and print something out. We're going to do the same again, but this time we'll do it from "scratch".
    
    The skeleton will just be a class with an empty main method. Your task is to add some code so that it reads in a String and a float, concatenates the float onto the String, then prints out the result. You may not have been explicitly taught every component of what you need to do, but you have seen them all.
    
    Try doing this without looking back at the previous code. You may find the auto complete feature handy in exploring what's possible, or you may want to dive into the API specification. There's also a couple of hints in the skeleton.
    
    If you get stuck, ask a question before just looking back at the code - the copy-paste approach to programming is useful at times, but it doesn't teach you anything if you don't already understand what you're copying, it should only be used when know what to do already.

---
+++ Programming 1/Lab-Work/Input Challenge.java
@@ -0,0 +1,21 @@
+//Don't forget something goes up here.
+import java.util.Scanner;
+public class ReadingStuff {
+	
+	public static void main(String[] args) {
+		//Read something... was there something
+		 Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		float b = sc.nextFloat();
+		String c = a+b;
+		System.out.println(c);
+		//that had to be done first?
+	
+
+		//Concatenate the two things.
+		
+		//Print it out.
+		
+	}
+	
+}
