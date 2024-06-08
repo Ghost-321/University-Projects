0e3d1357893e85737cfc39e4904fa9663302a0b0
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-08 16:40:26 +1000

    Create Input Challenge 2.java
    An Even Smaller Input Challenge
    This time we'll look at doing a similar thing in Python. Again you will (hopefully) notice that this sort of simple task is what Python is built to do, so it will feel a lot more lightweight (if you've programmed before though, you may want to think about where and why this kind of approach changes from an advantage to a disadvantage).
    
    The task here is to prompt the user with the phrase Enter a number: , read in a number, then print That number is even. if it is even, and That number is odd. if it is odd.

---
+++ Programming 1/Lab-Work/Input Challenge 2.java
@@ -0,0 +1,10 @@
+# There's no boiler plate, you can just start
+# writing code.
+a=int(input("Enter a number:"))
+    
+# Read in a number with the prompt. Do you have
+# to convert anything?
+if a%2 == 0:  # put proper a condition in, keep the colon :
+  print("That number is even")  # replace this
+else:
+    print ("That number is odd")  # replace this
