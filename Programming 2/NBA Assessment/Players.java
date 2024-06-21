94f9e0d824f6922cb1350e8c93f806024ce81e29
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2024-06-21 13:30:42 +1000

    Create Players.java

---
+++ Programming 2/NBA Assessment/Players.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Players {
+    private ArrayList<Player> Players;
+    public Players()
+    {
+    }
+    public ArrayList<Player> getPlayers() {
+        return this.players;
+    }
+}
